b324747e3a77c85313792d7b6dfebdd051d3638b
Zephys7 <bielvieirabraga170@gmail.com>
2025-07-20 14:51:52 -0400

    Add files via upload

---
+++ produtos.js
@@ -0,0 +1,51 @@
+const produtos = [
+  {
+    "id": "anel-solitario",
+    "nome": "Anel Solitário",
+    "preco": 110.0,
+    "imagem": "https://via.placeholder.com/400x400?text=Anel+Solitário",
+    "descricao": "Um clássico atemporal, o anel solitário é a representação máxima da elegância. Perfeito para ocasiões especiais ou para adicionar um toque de brilho ao dia a dia. Banhado a ouro 18k e com uma zircônia central de alta qualidade."
+  },
+  {
+    "id": "anel-coracao",
+    "nome": "Anel Coração",
+    "preco": 180.0,
+    "imagem": "https://via.placeholder.com/400x400?text=Anel+Coração",
+    "descricao": "Delicado e romântico, este anel em formato de coração é a escolha ideal para presentear ou se presentear. Simboliza amor e carinho em cada detalhe. Banhado a ouro 18k."
+  },
+  {
+    "id": "anel-aparador",
+    "nome": "Anel Aparador",
+    "preco": 135.5,
+    "imagem": "https://via.placeholder.com/400x400?text=Anel+Aparador",
+    "descricao": "O anel aparador é versátil e perfeito para ser combinado com outros anéis, como alianças ou solitários, criando um mix incrível. Cravejado com microzircônias que garantem um brilho sem igual."
+  },
+  {
+    "id": "pulseira-perolas",
+    "nome": "Pulseira com Pérolas",
+    "preco": 129.9,
+    "imagem": "https://via.placeholder.com/400x400?text=Pulseira+Pérolas",
+    "descricao": "A sofisticação das pérolas em uma pulseira delicada e elegante. Ideal para compor looks que vão do casual ao formal, adicionando um toque de classe."
+  },
+  {
+    "id": "pulseira-dourada",
+    "nome": "Pulseira Dourada",
+    "preco": 89.9,
+    "imagem": "https://via.placeholder.com/400x400?text=Pulseira+Dourada",
+    "descricao": "Estilo refinado para o dia a dia. Uma pulseira versátil que combina com tudo."
+  },
+  {
+    "id": "colar-perolas",
+    "nome": "Colar com Pérolas",
+    "preco": 149.9,
+    "imagem": "https://via.placeholder.com/400x400?text=Colar+Pérolas",
+    "descricao": "Suavidade e charme definem este colar de pérolas, um item essencial no porta-joias de toda mulher elegante."
+  },
+  {
+    "id": "brinco-perolas",
+    "nome": "Brinco com Pérolas",
+    "preco": 79.9,
+    "imagem": "https://via.placeholder.com/400x400?text=Brinco+Pérolas",
+    "descricao": "Um toque de suavidade e charme para suas orelhas. Perfeito para qualquer ocasião."
+  }
+];
